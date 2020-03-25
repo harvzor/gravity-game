30cb6e0b50df977d91b3a54b19d91a731ab7e89f
Harvey <harveyjwilliams@aol.co.uk>
2020-03-25 20:04:15 +0100

    Moved camera to player but broke movement.

--- Player/Player.cs
+++ Player/Player.cs
@@ -10,10 +10,18 @@ public class Player : RigidBody2D
 	[Export]
 	public int ClickRadius = 64;
 
+	[Export]
+	public float ZoomStep = 0.5f;
+
 	private Vector2 InitialPosition;
 	private Vector2? NewVelocity;
 	private Boolean Reset;
 	private Vector2 ScreenSize;
+	private Vector2? NewZoom;
+	private Vector2 MinZoom = new Vector2(
+		x: 0.5f,
+		y: 0.5f
+	);
 
 	private Boolean _Dragging;
 	private Boolean Dragging
@@ -32,6 +40,7 @@ public class Player : RigidBody2D
 	private Node2D Sprite => base.GetNode<Node2D>("Sprite");
 	private CollisionShape2D CollisionShape => base.GetNode<CollisionShape2D>("CollisionShape2D");
 	private Line Line => base.GetNode<Line>("Line");
+	private Camera2D Camera => base.GetNode<Camera2D>("Camera");
 
 	/// <summary>Calculate the firing of this item.</summary>
 	private void CalculateVelocityFromMouseDrag()
@@ -78,11 +87,26 @@ public class Player : RigidBody2D
 		// this.CollisionShape.Disabled = true;
 	}
 
+	private void Zoom(float delta)
+	{
+		var newZoom = new Vector2(
+			x: this.Camera.Zoom.x + this.ZoomStep * delta,
+			y: this.Camera.Zoom.y + this.ZoomStep * delta
+		);
+
+		if (newZoom < this.MinZoom)
+			newZoom = this.MinZoom;
+
+		this.NewZoom = newZoom;
+	}
+
 	public override void _Input(InputEvent inputEvent)
 	{
 		if (inputEvent is InputEventMouseButton mouseEvent)
 		{
-			if ((ButtonList)mouseEvent.ButtonIndex == ButtonList.Left)
+			var buttonIndex = (ButtonList)mouseEvent.ButtonIndex;
+
+			if (buttonIndex == ButtonList.Left)
 			{
 				if ((mouseEvent.Position - base.Position).Length() < this.ClickRadius)
 				{
@@ -105,6 +129,14 @@ public class Player : RigidBody2D
 					this.DragEndPosition = null;
 				}
 			}
+			else if (buttonIndex == ButtonList.WheelDown)
+			{
+				this.Zoom(delta: 1);
+			}
+			else if (buttonIndex == ButtonList.WheelUp)
+			{
+				this.Zoom(delta: -1);
+			}
 		}
 	}
 
@@ -117,6 +149,12 @@ public class Player : RigidBody2D
 
 		this.Line.DragCurrentPosition = this.DragCurrentPosition;
 		this.Line.DragEndPosition = this.DragEndPosition;
+
+		if (this.NewZoom != null && this.NewZoom.Value.x != this.Camera.Zoom.x)
+		{
+			// Smooth zoom.
+			this.Camera.Zoom = this.Camera.Zoom.MoveToward(this.NewZoom.Value, delta * 10);
+		}
 	}
 
 	public override void _IntegrateForces(Physics2DDirectBodyState state)
