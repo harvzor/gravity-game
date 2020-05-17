11e11569697b7454c9090457b7173c8164dd1e42
Harvey <harveyjwilliams@aol.co.uk>
2020-05-17 23:10:17 +0200

    Fixed storage on Android.

--- Storage/StorageManager.cs
+++ Storage/StorageManager.cs
@@ -7,7 +7,8 @@ using Newtonsoft.Json;
 // Vaguely based off http://codetuto.com/2017/08/save-load-game-godot-engine/
 public class StorageManager
 {
-    private string StoragePath = "res://storage.json";
+    // C:\Users\harvey\AppData\Roaming\Godot\app_userdata\Gravity
+    private string StoragePath = "user://storage.json";
 
     public StorageV1 Storage;
 
@@ -22,6 +23,8 @@ public class StorageManager
     public bool SaveExists()
     {
         var f = new File();
+
+        f.Open(this.StoragePath, File.ModeFlags.Read);
 
         var exists = f.FileExists(this.StoragePath);
 
