d6f51c9e7e68071cbc54c3803350784ec370a7bd
Harvey <harveyjwilliams@aol.co.uk>
2020-05-27 23:44:23 +0200

    Portal works but leaves a line where the player was.

--- Portal/Portal.cs
+++ Portal/Portal.cs
@@ -11,14 +11,27 @@ public class Portal : Area2D
     public override void _Ready()
     {
         this.Connect("body_entered", this, "OnBodyEntered");
+        this.Connect("body_exited", this, "OnBodyExited");
     }
 
 	public void OnBodyEntered(RigidBody2D body)
 	{
 		if (body is Player player)
 		{
-            if (player.CanTeleport)
+            if (player.CanTeleport == true)
+            {
                 player.Position = this.ConnectingPortal.Position;
+                player.CanTeleport = false;
+            }
+		}
+	}
+
+	public void OnBodyExited(RigidBody2D body)
+	{
+		if (body is Player player)
+		{
+            if (player.CanTeleport == false)
+                player.CanTeleport = true;
 		}
 	}
 }
